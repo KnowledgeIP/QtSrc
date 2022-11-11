2a2d8b987bbcec2711c731e7037b494dec7ec2e6
qiull <1053927633@qq.com>
2022-11-11 15:22:59 +0800

    hash

---
+++ keilkilll.bat
@@ -0,0 +1,13 @@
+del *.stash /s
+del Makefile /s
+del Makefile.* /s
+del *.user /s
+del ui_* /s
+del *.pro.user.* /s 
+del *.sln /s 
+del *.vcxproj /s 
+del *.filters /s 
+del *.log /s 
+del *.sdf /s
+del *.suo /s
+exit
